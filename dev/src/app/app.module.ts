0f783396a178e5ba905d2230a18da9daddd1b666
Jade Allen Cook <jadeallencook@admins-MacBook-Pro.local>
2018-07-25 20:23:38 -0600

    updated components

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SignUpComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
